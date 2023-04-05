de24a9663860b688efd71523ee814f8a590b0159
karishmaag <karishmaagopinathan@gmail.com>
2023-04-05 21:36:41 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import Destination from './Component/Destination';
+import About from './Component/About';
+import Contact from './Component/Contact';
+import RegistrationForm from './Component/RegistrationForm';
+import SubmittedForm from './Component/SubmittedForm';
 import './App.css';
+import Home from './Component/Home';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/destination">Destination</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact</Link>
+            </li>
+            <li>
+              <Link to="/registration">Registration Form</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/destination" element={<Destination />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/registration" element={<RegistrationForm />} />
+          <Route path="/submittedform" element={<SubmittedForm />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
